0b222cd07f5e51f5dec517a74a0a3570cb1c5abb
Michael <michw2014@gmail.com>
2020-09-11 16:24:36 +0200

    Implement basic logic

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,28 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as tc from '@actions/tool-cache'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    let installDir = tc.find('ISCC.exe', 'latest');
 
-    core.setOutput('time', new Date().toTimeString())
+    if (!installDir) {
+
+      core.info('Downloading ...');
+      let archivePath = await tc.downloadTool("https://github.com/VarChar42/setup-InnoSetup/raw/main/bin/setup.zip");
+
+      core.info('Extracting ...');
+      let extractDir = await tc.extractZip(archivePath, 'innosetup');
+
+
+      core.info('Adding to the cache ...');
+      installDir = await tc.cacheDir(extractDir, 'ISCC.exe', 'latest', 'x64');
+    }
+
+    core.addPath(installDir);
+
+
+
   } catch (error) {
     core.setFailed(error.message)
   }
